65be80511fb776cbe5bd26e3f5e208a172920434
injulme <injulme0309@naver.com>
2023-12-15 23:13:59 +0900

    create earth object

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
 
 window.addEventListener("load", () => {
   init();
@@ -13,21 +15,60 @@ function init() {
 
   document.body.appendChild(renderer.domElement);
 
+  /** texture loader */
+  const textureLoader = new THREE.TextureLoader();
+
+  /** scene */
   const scene = new THREE.Scene();
 
+  /** camera */
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     1,
     500
   );
+  camera.position.z = 130;
 
-  camera.position.z = 5;
+  /** orbit controls */
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.autoRotate = true;
+  controls.enableDamping = true;
+  controls.maxDistance = 50;
+  controls.minDistance = 30;
+
+  /** sphere (earth) */
+  const earthGeometry = new THREE.SphereGeometry(15, 64, 32);
+  const earthMaterial = new THREE.MeshStandardMaterial({
+    map: textureLoader.load("assets/earth-daymap.jpg"),
+    side: THREE.FrontSide,
+    opacity: 0.8,
+    transparent: true,
+  });
+  const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+  scene.add(earth);
+
+  /** sphere (cloud) */
+  const cloudGeometry = new THREE.SphereGeometry(16, 64, 32);
+  const cloudMaterial = new THREE.MeshStandardMaterial({
+    map: textureLoader.load("assets/earth-clouds.jpg"),
+    side: THREE.FrontSide,
+    opacity: 0.2,
+    transparent: true,
+  });
+  const cloud = new THREE.Mesh(cloudGeometry, cloudMaterial);
+  scene.add(cloud);
+
+  /** ambient light */
+  const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+  scene.add(ambientLight);
 
   render();
 
   function render() {
     renderer.render(scene, camera);
+
+    controls.update();
 
     requestAnimationFrame(render);
   }
@@ -40,7 +81,14 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     renderer.render(scene, camera);
+
+    controls.update();
   }
 
   window.addEventListener("resize", handleResize);
+
+  // const gui = new GUI();
+
+  // const lightGui = gui.addFolder("light");
+  // lightGui.add(ambientLight, "intensity");
 }
